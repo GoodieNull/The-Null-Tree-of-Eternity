6b1a7c1216e22cba99eb250f0c0bf97c9fac0605
GoodieNull <yellowyt0123@gmail.com>
2023-09-29 11:48:20 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,9 +42,9 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)   
+	let gain = new Decimal(1)
+	if (hasUpgrade('s', 11)) gain = gain.times(upgradeEffect('s', 11))   
 	effect(); {
-		if (hasUpgrade('s', 11)) gain = gain.times(upgradeEffect('s', 11))
         return player[this.layer].points.add(1).pow(0.5)
     }
     effectDisplay(); { return format(upgradeEffect(this.layer, this.id))+"s" } // Add formatting to the effect
