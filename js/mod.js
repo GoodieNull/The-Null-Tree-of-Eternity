7edf9a7e6087d59d6fe3282049c80c1389a65795
GoodieNull <yellowyt0123@gmail.com>
2023-10-10 19:03:44 +0800

    .

--- js/mod.js
+++ js/mod.js
@@ -43,8 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-    if (hasUpgrade('s', 11)) gain = gain.times(1.5),
-	
+    if (hasUpgrade('s', 11)) gain = gain.times(1.5);
 	if (hasUpgrade('s', 12)) gain = gain.times(upgradeEffect('s', 12))
 	return gain
 }
