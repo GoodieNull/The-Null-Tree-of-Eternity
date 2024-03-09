e02883e52af8ecd93860db5a39ece45c050c6607
GoodieNull <yellowyt0123@gmail.com>
2024-03-09 17:14:47 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -91,7 +91,7 @@ addLayer("p", {
 
     color: "#4287F5",                       // The color for this layer, which affects many elements.
     resource: "prestige points",            // The name of this layer's main prestige resource.
-    row: 0,                                 // The row this layer is on (0 is the first row).
+    row: 1,                                 // The row this layer is on (0 is the first row).
 
     baseResource: "points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
