ec669adc1b517173f27d1e777f0215701d400278
GoodieNull <yellowyt0123@gmail.com>
2024-03-09 17:12:52 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -19,8 +19,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Added the whole tree.<br>
+		- Added other things.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
